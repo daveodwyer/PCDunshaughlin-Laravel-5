7f203b1717baed2072cd1cc4d3d1e6ba14b7874f
vagrant <dodwyer91@gmail.com>
2016-01-28 19:00:28 +0000

    Update Meta tags

--- resources/views/public/includes/master.blade.php
+++ resources/views/public/includes/master.blade.php
@@ -1,6 +1,8 @@
 <!DOCTYPE>
 <html>
 	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title>{{ $page->title }} | PCDunshaughlin</title>
 		<link rel="stylesheet" href="{!! asset('css/bootstrap.min.css') !!}" />
 		{!! Minify::stylesheet('/css/custom.css') !!}
